224df502af6493967ca5ae83136cf6955841f904
Fernando Figueira <690686+fmarquesfilho@users.noreply.github.com>
2026-02-14 14:04:51 -0300

    fest: adiciona saudação de acordo com o horário do dia

--- 2-hello-world/hello.go
+++ 2-hello-world/hello.go
@@ -11,6 +11,23 @@ func Hello(name string) string {
 	return prefixoPortugues + name
 }
 
+func getTimeGreeting(hour int) string {
+	if hour >= 5 && hour < 12 {
+		return "Bom dia"
+	} else if hour >= 12 && hour < 18 {
+		return "Boa tarde"
+	}
+	return "Boa noite"
+}
+
+func HelloWithTime(name string, hour int) string {
+	if name == "" {
+		name = "Mundo"
+	}
+
+	return getTimeGreeting(hour) + ", " + name
+}
+
 func main() {
 	fmt.Println(Hello("mundo"))
 }
--- 2-hello-world/hello_test.go
+++ 2-hello-world/hello_test.go
@@ -11,14 +11,35 @@ func assertMessage(t testing.TB, got, want string) {
 
 func TestHello(t *testing.T) {
 	t.Run("cumprimentar pessoas", func(t *testing.T) {
-		got := Hello("Web II")
-		want := "Olá, Web II"
+		got := Hello("Maria")
+		want := "Olá, Maria"
 		assertMessage(t, got, want)
 	})
 
 	t.Run("usar 'Mundo' quando string for vazia", func(t *testing.T) {
 		got := Hello("")
 		want := "Olá, Mundo"
+		assertMessage(t, got, want)
+	})
+
+	t.Run("saudação de manhã", func(t *testing.T) {
+		hour := 9
+		got := HelloWithTime("Rodolfo", hour)
+		want := "Bom dia, Rodolfo"
+		assertMessage(t, got, want)
+	})
+
+	t.Run("saudação da tarde", func(t *testing.T) {
+		hour := 14
+		got := HelloWithTime("Maria", hour)
+		want := "Boa tarde, Maria"
+		assertMessage(t, got, want)
+	})
+
+	t.Run("saudação da noite", func(t *testing.T) {
+		hour := 20
+		got := HelloWithTime("João", hour)
+		want := "Boa noite, João"
 		assertMessage(t, got, want)
 	})
 }
