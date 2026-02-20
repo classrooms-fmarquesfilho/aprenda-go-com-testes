8d956842d8fb19e1b48c261bc0a883771d24ae52
Fernando Figueira <690686+fmarquesfilho@users.noreply.github.com>
2026-02-20 16:08:38 -0300

    cria tipo personalizado de erro

--- 4-ponteiros-erros/carteira.go
+++ 4-ponteiros-erros/carteira.go
@@ -2,26 +2,34 @@ package ponteiroserros
 
 import "fmt"
 
-type error interface {
-	Error() string
-}
-
 type Carteira struct {
 	saldo int
+}
+
+type ErroSaldoInsuficiente struct {
+	Solicitado int
+	Disponivel int
+}
+
+func (e ErroSaldoInsuficiente) Error() string {
+	return fmt.Sprintf("não é possível sacar %d, pois o saldo disponível é %d",
+		e.Solicitado, e.Disponivel)
 }
 
 func (c *Carteira) Depositar(quantia int) {
 	c.saldo += quantia
 }
 
-func (c Carteira) Saldo() int {
+func (c *Carteira) Saldo() int {
 	return c.saldo
 }
 
 func (c *Carteira) Sacar(quantia int) error {
 	if quantia > c.saldo {
-		return fmt.Errorf("não é possível sacar %d, pois o saldo é %d",
-			quantia, c.saldo)
+		return ErroSaldoInsuficiente{
+			Solicitado: quantia,
+			Disponivel: c.saldo,
+		}
 	}
 	c.saldo -= quantia
 	return nil
